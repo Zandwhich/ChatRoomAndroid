30bbd91c8cfa834f3dbb4f6d94318e8acdb594d7
Alex Zdanowicz <alex.zdanowicz@gmail.com>
2020-04-21 01:21:20 -0700

    Added some comments

--- app/src/main/java/com/example/alex/chatclient/MainActivity.java
+++ app/src/main/java/com/example/alex/chatclient/MainActivity.java
@@ -1,11 +1,22 @@
+/*
+ * Author: Alex Zdanowicz
+ */
+
 package com.example.alex.chatclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * Starting page for activity that will display chat
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Method called when activity is created
+     * @param savedInstanceState Passed in saved instances
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
